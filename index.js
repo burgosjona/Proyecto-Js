a33e3dd2304bc258e30981bb6444a0f125863306
Jonathan Burgos <burgosjona@gmail.com>
2023-02-09 22:28:51 -0300

    modificando pagina desde 0

--- index.js
+++ index.js
@@ -1,78 +1,16 @@
-//Declaramos articulos con una constante (ya que el precio del articulo no cambia)
-const producto1 = "Alimento gatos MichiFeliz x 3 kg";
-const precio1 = 600;
-const producto2 = "Alimento gatos Catshus x 3 Kg";
-const precio2 = 500;
-const producto3 = "Alimento gatos Gatimix 9 x 9 Kg";
-const precio3 = 1100;
-//carrito de compras
-let total = 0;
-let continuarcompra = true;
-//la funcion SUMA
-function sumar(total, precio) {
-  return total + precio;
-}
-//le mostramos al usuario un saludo cordial y le pedimos una respuesta
-let seleccion = prompt(
-  "Bienvenido/a a Milu Pet Shop, desea comprar algun producto ingrese si o no?"
-);
-//si el usuario se equivoca de respuesta le pedimos que la ingrese nuevamente
-while (seleccion != "si" && seleccion != "no") {
-  alert("Por favor ingresa si o no");
-  seleccion = prompt("Si desea comprar algun producto ingrese si o no");
-}
-//si dice que si le mostramos el menu
-if (seleccion == "si") {
-  alert("Genial! A contunuacion nuestros productos.");
-}
-//si el usuario no quiere comprar nada le mostramos el monto y lo despedimos
-else if (seleccion == "no") continuarcompra = false;
+//declaramos contantes en objetos dentro de un array
+const productos = [
+  { Nombre: "Alimento de gato", precio: 200 },
+  { Nombre: "Alimento de Perro", precio: 400 },
+  { Nombre: "Piedritas", precio: 500 },
+  { Nombre: "juguete", precio: 300 },
+];
+//creamos una variable carrito para acumular los productos
+let carrito = [];
+//una variable para que el usuario elija el articulo
+let seleccion = prompt("Hola desea comprar algun articulo si o no?");
 
-//mostramos el listado al usuario
-while (continuarcompra) {
-  let userChoice = prompt(
-    "Seleccione un producto: 1. " +
-      producto1 +
-      " - $" +
-      precio1 +
-      " 2. " +
-      producto2 +
-      " - $" +
-      precio2 +
-      " 3. " +
-      producto3 +
-      " - $" +
-      precio3
-  );
-  //si el usuario elege opc 1 entonces sumamos producto 1 + precio 1
-  if (userChoice == 1) {
-    total = sumar(total, precio1);
-    console.log("Tu has elegido: " + producto1 + " - $" + precio1);
-  }
-  //si el usuario elige opc 2 entonces le sumamos prod 2 + precio 2
-  else if (userChoice == 2) {
-    total = sumar(total, precio2);
-    console.log("Tu has elegido: " + producto2 + " - $" + precio2);
-  }
-  //si el usuario elige opc 3 entonces le sumamos el prod 3 + precio 3
-  else if (userChoice == 3) {
-    total = sumar(total, precio3);
-    console.log("Tu has elegido: " + producto3 + " - $" + precio3);
-  }
-  //si el usuario se equivoca de opc le pedimos que ingres nuevamente la opcion
-  else {
-    alert("Invalido. Por favor, elija un número entre 1 y 3.");
-  }
-  console.log("Actual total: $" + total);
-  //preguntamos al usuario si va a seguir comprando o no
-  let sigecomprando = prompt(
-    "Queres seguir comprando? Escribe 'si' o 'no'"
-  ).toLowerCase();
-  if (sigecomprando === "no") {
-    continuarcompra = false;
-  }
+while (seleccion != "si" && seleccion != "no") {
+  alert("Error, por favor ingresa si o no");
+  seleccion = prompt("Hola  desea comprar algun articulo si o no?");
 }
-//si no va a seguir comprando pasamos el total de la compra.
-alert("Total: $" + total);
-//saludamos epicamente.
-alert("Gracias por comprar! vuelva prontos.");
